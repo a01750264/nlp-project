68d5090210aea20389f7e14a0e3d688ba26f3e19
Mikaela Grace <mikaelagrace@users.noreply.github.com>
2022-11-17 23:52:14 -0500

    Update translation_models.py
    No need to merge the Pull Request if you don't want to, it's just a way for me to suggest updates with inline diffs!

--- translation_models.py
+++ translation_models.py
@@ -12,6 +12,8 @@ class Translations:
         Constructor of the class, receives the file to be translated, the file to evaluate
         the bleu of the translation and the Deepl API key
         '''
+        # https://towardsdatascience.com/whats-the-meaning-of-single-and-double-underscores-in-python-3d27d57d6bd1
+        # single underscore not double! 
         self.__source_file = source_file
         self.__target_file = target_file
         self.__deepl_key = deepl_key
@@ -25,17 +27,17 @@ class Translations:
         translations
         '''
         translator = deepl.Translator(self.__deepl_key)
-        n = 0
-        with open(self.__source_file, 'r', encoding='utf-8') as s:
+        num_lines = 0
+        with open(self.__source_file, 'r', encoding='utf-8') as s: # single-letter variable names are discouraged! 
             with open(self.__target_file, 'r', encoding='utf-8') as t:
                 for line in s:
                     ref = [t.readline().split()]
                     translation = translator.translate_text(line, source_lang='ES', target_lang='EN-US')
                     result = str(translation).split()
                     self.__deepl_bleu += sentence_bleu(ref, result, (1, 0, 0, 0))
-                    n += 1
-                t.close()
-            s.close()
+                    num_lines += 1
+                    
+        # when you use "with open(", it closes the file for you! https://www.programiz.com/python-programming/file-operation
         print(f'DEEPL_TRANSLATOR: {self.__deepl_bleu/n}')
 
 
@@ -44,6 +46,9 @@ class Translations:
         Runs the translation with Google's translator and computes the average bleu of the
         translations
         '''
+        # same changes from above apply here! 
+        # you could refactor to have a function that gets the translations as a list of texts, and then another that computes the
+        # bleu score that gets called in both places, to avoid repeating so much code. 
         translator = Translator()
         n = 0
         with open(self.__source_file, 'r', encoding='utf-8') as s:
