7503896287343cf1c6fb72d226fc917072d3dca8
Mikaela Grace <mikaelagrace@users.noreply.github.com>
2022-11-17 23:48:33 -0500

    Update sentiment_analysis.py
    No need to merge the Pull Request if you don't want to, it's just a way for me to suggest updates with inline diffs!

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,6 +1,15 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline, logging
 
 
+
+# Dont need to do this for the homework, but just FYI and as a way to improve your code in general: 
+# https://docs.python.org/3/library/typing.html
+# Adding typing can really help with catching small errors! 
+# 
+# Also, using black for autoformatting: 
+# https://pypi.org/project/black/
+
+
 class SentimentAnalysis:
     '''
     Class that solves the first problem
@@ -11,6 +20,7 @@ class SentimentAnalysis:
         Constructor of the class, receives the model to be used and the data to be analyzed
         '''
         logging.set_verbosity_error()
+        # should be single underscores for the next 4 lines: https://towardsdatascience.com/whats-the-meaning-of-single-and-double-underscores-in-python-3d27d57d6bd1
         self.__model = AutoModelForSequenceClassification.from_pretrained(model)
         self.__tokenizer = AutoTokenizer.from_pretrained(model)
         self.__file = file
@@ -29,5 +39,13 @@ class SentimentAnalysis:
 
 # TESTS
 if __name__ == '__main__':
+    
+"""Be sure to add tests for all pieces of functionality! 
+Tests should check that the functionality/outputs are as expected, NOT just that the code runs! 
+When writing code, I write the test for each class or piece of functionality as soon as I finish that piece.
+It helps you develop incrementally, being sure that each piece of code is clean and works like you expect it to! 
+for tests, best practices are to have a structure like this: 
+https://stackoverflow.com/questions/1896918/running-unittest-with-typical-test-directory-structure
+"""
     s = SentimentAnalysis('cardiffnlp/twitter-roberta-base-sentiment-latest', 'tiny_movie_reviews_dataset.txt')
     s.run()
